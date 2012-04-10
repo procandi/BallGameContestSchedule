8378696f6022fcda20404eeb7aad427aa3975ec8
YingHua Hsieh <promiseway@hotmail.com>
2012-04-11 03:54:01 +0800

    repush lastest version.[*]

---
+++ BallGameContestSchedule/export_schedule.py
@@ -0,0 +1,67 @@
+﻿# -*- coding: utf-8 -*-
+
+import re
+import sys
+import datetime
+
+import win32com.client
+import win32ui
+
+#my self module to process some question
+import modUserInfo
+
+
+#load base data
+userinfo=modUserInfo.modUserInfo()
+userinfo.loadbaseinfo()
+
+#get begin date
+begin_date = datetime.date(int(userinfo.begin_year),int(userinfo.begin_month),int(userinfo.begin_day))
+delta = datetime.timedelta(days=7)
+
+now=begin_date
+#nowarr=str(now).split('-')
+
+next=now+delta
+#nextarr=str(next).split('-')
+
+#get a day game count
+count=14
+
+k=0	#field number
+m=0	#day numer
+n=0	#round number
+for i in range(userinfo.team_count):
+	for j in range(userinfo.team_count):
+		if i<j and userinfo.team[i].name!=userinfo.team[j].name:
+			userinfo.field[i].datetag[m]=now
+			userinfo.field[k].date[m].teamA[n]=userinfo.team[i]
+			userinfo.field[k].date[m].teamB[n]=userinfo.team[j]
+			
+			if k<userinfo.field_count:
+				k+=1	#if field is not use end, then use other field to start contest.
+			else:
+				if n<count:
+					k=0		#if all field one round is full, then user other rounds.
+					n+=1
+				else:
+					k=0		#if all field and all round is full, then user other days.
+					m+=1
+					n=0
+					now=next	#update date information
+					next=next+delta					
+			
+			print userinfo.field[i].datetag[m]
+			print userinfo.team[i].name.encode("big5") + " VS " + userinfo.team[j].name.encode("big5")
+
+#app = win32com.client.Dispatch("Excel.Application")
+#app.visible = True
+#workbook = app.Workbooks.Add()
+#workbook.Sheets[0].Name = "test"
+#sheet = workbook.Sheets[0]
+#sheet.Range("A1").Value = ("this is A1"+userinfo.field[1].encode("big5"))
+#sheet.Cells(3, 2).Value = "Hi Alarm"  #==B3
+#sheet.Cells(3, 2).AddComment()
+#sheet.Cells(3, 2).Comment.Shape.Width = 150
+#sheet.Cells(3, 2).Comment.Shape.Height = 160
+#sheet.Cells(3, 2).Comment.Text("this is memo")
---
+++ BallGameContestSchedule/modUserInfo.py
@@ -0,0 +1,78 @@
+﻿# -*- coding: utf-8 -*-
+
+import re
+
+#my self module to process some question
+import modFile
+import modPublic
+
+class modUserInfo:
+	def __init__(self):
+		pass
+	
+	def loadbaseinfo(self):
+		#load file body
+		file=modFile.modFile()
+		result=file.readfile("fielddata.txt")
+		
+		#made field array
+		self.field=range(100)
+		for i in range(100):
+			self.field[i]=modPublic.sfield()
+			for j in range(100):
+				self.field[i].date[j]=modPublic.sschedule()
+				
+		#add field data to origin data
+		line=result.split("\r\n")
+		i=0
+		for v in line:
+			v=re.sub(r"field.+=","",v)
+			self.field[i].name=v
+			i+=1
+		self.field_count=i
+		
+		
+		#load file body
+		file=modFile.modFile()
+		result=file.readfile("teamdata.txt")
+		
+		#made team struct and array
+		self.team=range(100)
+		for i in range(100):
+			self.team[i]=modPublic.steam()
+		
+		#add team data to origin data
+		line=result.split("\r\n")
+		i=0
+		for v in line:
+			if v is None:
+				break
+			if v=="":
+				break
+			
+			v=re.sub(r"team.+=","",v)
+			vd=v.split('-')
+			va0=vd[0].split(',')
+			va1=vd[1].split(',')
+			
+			self.team[i].ingame=va0[0]
+			self.team[i].name=va0[1]
+			self.team[i].field=va0[2]
+			self.team[i].good=va0[3]
+			for j in range(100):
+				if j<len(va1)-1:	#need subtract one, because date value will have one blank data.
+					self.team[i].schedule[j]=va1[j]
+				else:
+					break
+			i+=1
+		self.team_count=i
+		
+		
+		#load file body
+		file=modFile.modFile()
+		result=file.readfile("datedata.txt")
+		
+		v=result.split('/')
+		self.begin_year=v[0]
+		self.begin_month=v[1]
+		self.begin_day=v[2]
