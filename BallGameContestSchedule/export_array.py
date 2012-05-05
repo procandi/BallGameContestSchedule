02fe96bb3b2abe792bbc067244b5abf9e7695847
YingHua Hsieh <promiseway@hotmail.com>
2012-05-06 00:54:09 +0800

    new algorothm is begin coding.

--- BallGameContestSchedule/export_array.py
+++ BallGameContestSchedule/export_array.py
@@ -13,87 +13,148 @@ import modUserInfo
 import modOffice
 import modPublic
 
-
-#load template and base data
-userinfo=modUserInfo.modUserInfo()
-userinfo.loadbaseinfo(3)
-userinfo.loadtemplate()
-
-#get begin date
-begin_date = datetime.date(int(userinfo.begin_year),int(userinfo.begin_month),int(userinfo.begin_day))
-delta = datetime.timedelta(days=7)
-now=begin_date
-#nowarr=str(now).split('-')
-next=now+delta
-#nextarr=str(next).split('-')
-
-#make array
-arr=modPublic.sarray()
-for i in range(100):
-	for j in range(100):
-		arr.teamA[i][j]=""
-		arr.teamB[i][j]=""
-		arr.black[i][j]=False
-		arr.isOver[i][j]=False
-
-#sort game
-k=0	#field number
-d=0	#day numer
-n=0	#round number
-for i in range(userinfo.team_count):
-	for j in range(userinfo.team_count):
-		if i<j and userinfo.team[i].name!=userinfo.team[j].name:
-			userinfo.field[k].date[d]=now
-			userinfo.field[k].teamA[d][n]=userinfo.team[i].name
-			userinfo.field[k].teamB[d][n]=userinfo.team[j].name
-
-			print str(k)+" "+str(d)+" "+str(n)
-			print userinfo.field[i].date[d]
-			print userinfo.team[i].name + " VS " + userinfo.team[j].name
+class export_array:
+	def __init__(self):
+		#load template and base data
+		self.userinfo=modUserInfo.modUserInfo()
+		self.userinfo.loadbaseinfo(3)
+		self.userinfo.loadtemplate()
+		
+		#get begin date
+		begin_date = datetime.date(int(self.userinfo.begin_year),int(self.userinfo.begin_month),int(self.userinfo.begin_day))
+		delta = datetime.timedelta(days=7)
+		now=begin_date
+		#nowarr=str(now).split('-')
+		next=now+delta
+		#nextarr=str(next).split('-')
+		
+		#made 2*2 matrix array to sort
+		self.arr=modPublic.sarray()
+		for i in range(100):
+			for j in range(100):
+				self.arr.teamA[i][j]=""
+				self.arr.teamB[i][j]=""
+				self.arr.black[i][j]=False
+				self.arr.isOver[i][j]=False
+		
+		#sort game
+		k=0	#field number
+		d=0	#day numer
+		n=0	#round number
+		for i in range(self.userinfo.team_count):
+			for j in range(self.userinfo.team_count):
+				if i<j:
+					self.userinfo.field[0].date[d]=now
+					self.arr.teamA[i][j]=self.userinfo.team[i].name
+					self.arr.teamB[i][j]=self.userinfo.team[j].name
+					
+					#round first
+					if n<int(self.userinfo.count):
+						n+=1	#if round is not use end, then use other round to start contest.
+					else:
+						n=0		#if all field and all round is full, then user other days.
+						d+=1
+						now=next	#update date information
+						next=next+delta	
+						
+					print i,j
+					print str(d)+" "+str(n)
+					print self.userinfo.field[0].date[d]
+					print self.userinfo.team[i].name + " VS " + self.userinfo.team[j].name
+		
+		
+		#d is need skip.#
+		self.maxd=d+1
+		
+		#enum array
+		self.enumarray_main()
+		
+		
+		#create excel
+		office=modOffice.modOffice()
+		office.create_excel_app()
+		office.create_excel_workbook()
+		office.create_excel_sheets()
+		
+		#set row and col
+		row=1
+		col=1
+		
+		for i in range(self.maxd):    #day number
+			#set excel title
+			v=re.sub(r"\d","",str(self.userinfo.field[0].date[i]))
+			if v!="":
+				office.sheet.Cells(row, col).Value=self.userinfo.field[0].date[i]
+				office.sheet.Cells(row, col+1).Value=self.userinfo.field[0].name
+			row+=1
 			
-			#round first
-			if n<count:
-				n+=1	#if round is not use end, then use other round to start contest.
+			#set excel value
+			for k in range(int(self.userinfo.count)):    #round number
+				v=re.sub(r"\d","",str(self.userinfo.field[0].teamA[i][k]))
+				if v!="":
+					if k<3:
+						office.sheet.Cells(row, col).Value=("0"+str(k+7)+":30")
+					else:
+						office.sheet.Cells(row, col).Value=(str(k+7)+":30")
+					office.sheet.Cells(row, col+1).Value=self.userinfo.field[0].teamA[i][k]
+					office.sheet.Cells(row, col+2).Value=self.userinfo.field[0].teamB[i][k]
+					row+=1
+	
+		
+	def enumarray_sub(self,arrow,x,y,addedx,addedy,takeout,day):
+		if self.arr.isOver[x][y]==False and self.arr.teamA[x][y]!="" and self.arr.teamB[x][y]!="":	#if this cell is can be choice, then save it and acount added number by arrow.
+			self.arr.isOver[x][y]=True
+			takeout+=1
+			self.userinfo.field[0].teamA[day][takeout]=self.arr.teamA[x][y]
+			self.userinfo.field[0].teamB[day][takeout]=self.arr.teamB[x][y]
+			if arrow=="y":
+				addedy+=1
 			else:
-				if k<userinfo.field_count-1:
-					n=0		#if one field all round is full, then user other fields.
-					k+=1
+				addedy=1
+			print takeout,self.arr.teamA[x][y],self.arr.teamB[x][y]
+		elif addedy>=2 and arrow=="x":	#if it is change row, then this should clear y count.
+			addedy=0
+		
+		if takeout<int(self.userinfo.count) and x<int(self.userinfo.team_count):	#if take out is not enough.
+			print "in",x,y,addedx,addedy
+			if addedy<2 and y<int(self.userinfo.team_count):	#if this row can be choice, and y is not over array length.
+				self.enumarray_sub("y",x,y+1,addedx,addedy,takeout,day)
+			else:	#if this row is take out more then rule, or this y is over then array length.
+				self.enumarray_sub("x",x+1,0,addedx,addedy,takeout,day)
+		#else:	#if x is over then array, then restart it
+			#self.enumarray_sub("x",0,0,addedx,addedy,takeout,day)
+	
+	def enumarray_main(self):
+		#output matrix
+		for j in range(self.userinfo.team_count):
+			for i in range(self.userinfo.team_count):
+				if self.arr.isOver[i][j]==False and self.arr.teamA[i][j]!="" and self.arr.teamB[i][j]!="":
+					print self.arr.teamA[i][j],"-",self.arr.teamB[i][j],",",
 				else:
-					n=0		#if all field and all round is full, then user other days.
-					d+=1
-					k=0
-					now=next	#update date information
-					next=next+delta	
-#d is need skip.#
+					print "  -   ,",
+			print
+			
+		#enum all result util no anyone is not choice
+		d=0
+		while True:
+			sum=0
+			for i in range(self.userinfo.team_count):
+				for j in range(self.userinfo.team_count):
+					if self.arr.isOver[i][j]==False and self.arr.teamA[i][j]!="" and self.arr.teamB[i][j]!="":
+						sum+=1
+			
+			if sum>=int(self.userinfo.count):
+				self.enumarray_sub("y",0,0,0,0,0,d)
+				d+=1
+			else:
+				t=0
+				for i in range(self.userinfo.team_count):
+					for j in range(self.userinfo.team_count):
+						if self.arr.isOver[i][j]==False and self.arr.teamA[i][j]!="" and self.arr.teamB[i][j]!="":
+							self.userinfo.field[0].teamA[d][t]=self.arr.teamA[i][j]
+							self.userinfo.field[0].teamB[d][t]=self.arr.teamB[i][j]
+							t+=1
+							self.arr.isOver[i][j]=True
+				break
 
-
-#create excel
-#office=modOffice.modOffice()
-#office.create_excel_app()
-#office.create_excel_workbook()
-#office.create_excel_sheets()
-
-#set row and col
-#row=1
-#col=1
-
-#for i in range(d+1):    #day number
-#	for j in range(userinfo.field_count):    #field number
-#		#set excel title
-#		v=re.sub(r"\d","",str(userinfo.field[j].date[i]))
-#		if v!="":
-#			office.sheet.Cells(row, col).Value=userinfo.field[j].date[i]
-#			office.sheet.Cells(row, col+1).Value=userinfo.field[j].name
-#		row+=1
-#		
-#		#set excel value
-#		for k in range(count):    #round number
-#			v=re.sub(r"\d","",str(userinfo.field[j].teamA[i][k]))
-#			if v!="":
-#				if k<3:
-#					office.sheet.Cells(row, col).Value=("0"+str(k+7)+":30")
-#				else:
-#					office.sheet.Cells(row, col).Value=(str(k+7)+":30")
-#				office.sheet.Cells(row, col+1).Value=userinfo.field[j].teamA[i][k]
-#				office.sheet.Cells(row, col+2).Value=userinfo.field[j].teamB[i][k]
-#				row+=1
+export_array()
---
+++ BallGameContestSchedule/modAlogorithm.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+
+#my self module to process some question
+import modPublic
+
+class modAlogorithm:
+	def __init__(self):
+		pass
+	
+	def enumarray_sub(self):
+		print self.arr.isOver[0][0]
+	
+	def enumarray_main(self):
+		#make array
+		self.arr=modPublic.sarray()
+		for i in range(100):
+			for j in range(100):
+				self.arr.teamA[i][j]=""
+				self.arr.teamB[i][j]=""
+				self.arr.black[i][j]=False
+				self.arr.isOver[i][j]=False
+		
+		self.enumarray_sub()
+		self.arr.isOver[0][0]=True
+
